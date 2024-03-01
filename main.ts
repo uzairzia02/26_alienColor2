134414d6b5a5ff0e5532c0425cd2997e9e4588ba
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 22:50:28 +0500

    initial commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+let aliencolour2: string= 'green';
+
+
+//running the if condition code
+if(aliencolour2==='green'){
+    console.log("You earned 5 points for shooting the alien");
+}
+else {
+console.log("You earned 10 points for shooting the alien");
+}
+
+
+//running the else condition code
+aliencolour2 = 'blue';
+if(aliencolour2==='green'){
+    console.log("\nYou earned 5 points for shooting the alien");
+}
+else {
+console.log("\nYou earned 10 points for shooting the alien");
+}
